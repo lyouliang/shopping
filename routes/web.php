7bec2312128f407346f731823f3a1178cb50803f
bobbylow <bobbylow@pop-os.localdomain>
2025-06-25 06:48:20 +0800

    add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ShopController;
 
-Route::get('/', [ShopController::class, 'login']);
+Route::get('/', [ShopController::class, 'login'])->name('login');
 
-Route::get('shop', [ShopController::class, 'index'])->middleware(['auth'])->name('shop.index');
+Route::get('login',  [ShopController::class, 'login'])->name('login');
 
-Route::post('cart', [CartController::class, 'createcart'])->name('cart.create');
+Route::middleware('auth')->group(function () {
+    Route::get('shop', [ShopController::class, 'index'])->name('shop.index');
 
-Route::get('cart', [CartController::class, 'cartIndex'])->name('cart.get');
+    Route::post('cart', [CartController::class, 'createcart'])->name('cart.create');
 
-Route::get('login', [ShopController::class, 'login']);
-
-Route::post('login', [LoginController::class, 'authenticate']);
-
-Route::get('login/success', [ShopController::class, 'loginSuccess'])->name('login.success');
-
-Route::get('register', [ShopController::class, 'register']);
-
-// Route::get('dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::get('/cart', [CartController::class, 'show']);
-
-// require __DIR__.'/settings.php';
-// require __DIR__.'/auth.php';
+    Route::get('cart', [CartController::class, 'cartIndex'])->name('cart.get');
+});
